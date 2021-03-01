acc7d29e38f10aaea14a1d26a2cca7dfa774c0fa
Michael J. Ryan <tracker1@gmail.com>
2021-02-28 17:00:44 -0800

    add ufw-config example, force nightly rebuild

---
+++ ufw-config.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+ufw allow 80
+ufw allow 443
+ufw allow 1123
+ufw allow 11235
+ufw allow 21
+ufw allow 22
+ufw allow 23
+ufw allow 513
+ufw allow 64
+ufw allow 128
+ufw allow 25
+ufw allow 587
+ufw allow 465
+ufw allow 110
+ufw allow 995
+ufw allow 119
+ufw allow 563
+ufw allow 18
+ufw allow 11
+ufw allow 17
+ufw allow 79
+ufw allow 6667
